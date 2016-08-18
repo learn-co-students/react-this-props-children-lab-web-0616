4dbc7e6ccbfcf32897b69aa529fdb130781691a4
thomastuts <thomas@thomastuts.com>
2016-08-18 15:30:25 +0200

    Add instructions for lab

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 
 const Invitation = require('./components/Invitation');
-const ThemedParty = require('./components/ThemedParty');
+const ThemedParty = require('./components/ThemedDecorations');
 
 ReactDOM.render(
-  <Invitation/>,
+  <div>
+    <Invitation>Hey, you should totally come to this awesome party I'm throwing!</Invitation>
+    <ThemedParty theme="heaven">
+      <p>A decoration</p>
+      <p>Some other decoration</p>
+      <p>A last decoration</p>
+    </ThemedParty>
+  </div>,
   document.getElementById('main')
 );
